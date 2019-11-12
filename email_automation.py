dce8add1a020a79fa7fad493023d5a4f6db808ca
Javier Legido <javier.legido@kedu.coop>
2019-11-12 10:17:47 +0100

    Fixed bug not adding to processed_emails all of them

--- email_automation.py
+++ email_automation.py
@@ -102,8 +102,7 @@ class Email():
                     'body': email_body.decode('utf8'),
                 }
                 self.pending_processing_email.append(d_email)
-            elif not self._is_acl_from_email_address(email_from):
-                self.mark_email_as_processed(email_id)
+            self.mark_email_as_processed(email_id)
         return self.pending_processing_email
 
     def mark_email_as_processed(self, email_id):
